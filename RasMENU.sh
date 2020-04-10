b5103baf99b32fa1885d92fb60485da9a14cbfbb
TANEKEN <taneken2000@gmail.com>
2020-04-10 23:44:45 +0900

    エラールーチン追加。ISOのeject対応など。細々.

--- RasMENU.sh
+++ RasMENU.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# RaMENU Ver.0.1d
+# RaMENU Ver.0.1e
 #   RaSCSI Image Mount Support Tool
 #     Copyright (C) 2020 @taneken2000
 
@@ -19,7 +19,7 @@ do
 
 #配列初期化
 SCSI_ID=(0 1 2 3 4 5 6)
-SCSI_HDD=("none" "none" "none" "none" "none" "none" "bridge")
+SCSI_HDD=("none" "none" "none" "none" "none" "none" "none")
 files=()
 IMAGE=()
 
@@ -27,14 +27,21 @@ IMAGE=()
 # SCSI list
 #############
 IFS=$'\n'
-for r  in `rasctl -l`
+for r  in `rasctl -l 2>&1`
 do
-    if [ ${r:0:1}  == "+" ]; then
+    if [ ${r:0:5} == "Error" ]; then        # rascsiが起動していないとき
+        whiptail --msgbox "${r}" 0 0 3>&2 2>&1 1>&3-
+        exit 0
+    fi
+    if [ "${r:0:2}" == "No" ]; then # ひとつもマウントされてないとき
+        break;
+    fi
+    if [ ${r:0:1} == "+" ]; then
         :   #破棄
-    elif [ ${r:0:4}  == "| ID" ]; then
+    elif [ ${r:0:4} == "| ID" ]; then
         :   #破棄
     else
-        SCSI_HDD[${r:3:1}]="${r:19:30}"
+        SCSI_HDD[${r:3:1}]="${r:19}"
     fi
 done
 
@@ -61,33 +68,76 @@ fi
 #############
 # image list
 #############
-i=0
-#for f in `find ${IMAGE_PATH} -iname "*.HDS" | sort`
-for f in `find ${IMAGE_PATH} \( -iname "*.HD[FSNAI]" -o -iname "*.NHD" -o -iname "*.MOS" -o -iname "*.ISO" \) | sort`
-do
-    IMAGE[i]="${f}"
-    ((i++))
-    IMAGE[i]="${f}"
-    ((i++))
-done
 
 #指定したSCSI-IDにすでにマウント済だったらイジェクトを一番上に表示する
-if [ ${SCSI_HDD[$RESID]} == "none" ]; then
-    :
+if [ ${SCSI_HDD[$RESID]} = "none" ]; then
+    i=0
+    for f in `find ${IMAGE_PATH} \( -iname "*.HD[FSNAI]" -o -iname "*.NHD" -o -iname "*.MOS" -o -iname "*.ISO" \) | sort`
+    do
+        IMAGE[i]="${f}"
+        ((i++))
+        IMAGE[i]="${f}"
+        ((i++))
+    done
+    IMAGE=(${IMAGE[@]} "<BRIDGE>" "<BRIDGE>")
+elif [ ${SCSI_HDD[$RESID]} = "NO MEDIA" ]; then
+    i=0
+    for f in `find ${IMAGE_PATH} -iname "*.ISO" | sort`
+    do
+        IMAGE[i]="${f}"
+        ((i++))
+        IMAGE[i]="${f}"
+        ((i++))
+    done
+    IMAGE=("<DETACH>" "<DETACH>" ${IMAGE[@]})
+elif [ ${SCSI_HDD[$RESID]##*.} = "ISO(WRITEPROTECT)" ] || [ ${SCSI_HDD[$RESID]##*.} = "iso(WRITEPROTECT)" ]; then
+    i=0
+    for f in `find ${IMAGE_PATH} -iname "*.ISO" | sort`
+    do
+        IMAGE[i]="${f}"
+        ((i++))
+        IMAGE[i]="${f}"
+        ((i++))
+    done
+    IMAGE=("<DETACH>" "<DETACH>" "<EJECT>" "<EJECT>" ${IMAGE[@]})
+elif [ ${SCSI_HDD[$RESID]##*.} = "MOS(WRITEPROTECT)" ] || [ ${SCSI_HDD[$RESID]##*.} = "mos(WRITEPROTECT)" ] | [ ${SCSI_HDD[$RESID]##*.} = "MOS" ] || [ ${SCSI_HDD[$RESID]##*.} = "mos" ]; then
+    i=0
+    for f in `find ${IMAGE_PATH} -iname "*.MOS" | sort`
+    do
+        IMAGE[i]="${f}"
+        ((i++))
+        IMAGE[i]="${f}"
+        ((i++))
+    done
+    IMAGE=("<DETACH>" "<DETACH>" "<EJECT>" "<EJECT>" "<PROTECT>" "<PROTECT>" ${IMAGE[@]})
 else
-    IMAGE=("<EJECT>" "<EJECT>" ${IMAGE[@]})
+    i=0
+    for f in `find ${IMAGE_PATH} \( -iname "*.HD[FSNAI]" -o -iname "*.NHD" -o -iname "*.MOS" -o -iname "*.ISO" \) | sort`
+    do
+        IMAGE[i]="${f}"
+        ((i++))
+        IMAGE[i]="${f}"
+        ((i++))
+    done
+    IMAGE=("<DETACH>" "<DETACH>" ${IMAGE[@]})
 fi
 
 #イメージ選択画面表示
-RESFILE=$(whiptail --notags --title "Select IMAGE FILE" --backtitle "$TTL" --menu "`rasctl -l`" 0 0 0 ${IMAGE[@]} 3>&2 2>&1 1>&3-)
+RESFILE=$(whiptail --notags --title "Select IMAGE FILE" --backtitle -"$TTL" --menu "`rasctl -l`" 0 0 0 ${IMAGE[@]} 3>&2 2>&1 1>&3-)
 RET=$?
 #echo $RET
 #echo "ID:"$RESID "FILE:"$RESFILE
 if [ $RET -eq 255 ]; then
     :
 else
-    if [ $RESFILE == "<EJECT>" ]; then
+    if [ $RESFILE == "<DETACH>" ]; then
         rasctl -i "$RESID" -c detach
+    elif [ $RESFILE == "<EJECT>" ]; then        # .MOSのinsertの配慮
+        rasctl -i "$RESID" -c eject
+    elif [ $RESFILE == "<PROTECT>" ]; then
+        rasctl -i "$RESID" -c protect
+    elif [ $RESFILE == "<BRIDGE>" ]; then
+        rasctl -i "$RESID" -c aetach -t bridge
     else
         rasctl -i "$RESID" -c attach -f "$RESFILE"
     fi
